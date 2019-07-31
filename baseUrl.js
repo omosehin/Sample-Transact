c72d8fdb7c39ae674903d8227d44cecb38d7e56c
omosehin <omosehin14@gmail>
2019-07-31 11:43:59 +0100

    add delete and view button

--- baseUrl.js
+++ baseUrl.js
@@ -1,1 +1,3 @@
-export const baseUrl =" http://5d2c2f2b8c90070014972225.mockapi.io/api/v2/";
+const baseUrl =" http://5d2c2f2b8c90070014972225.mockapi.io/api/v2/";
+
+module.exports.baseUrl=baseUrl;
--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const baseUrl = " http://5d2c2f2b8c90070014972225.mockapi.io/api/v2/";
+// const baseUrl = require('./baseUrl')
 
-const mountains = [{ Title: "", Url: "1654", Avatar: "Parco", Action: "" }];
+let errorId = document.getElementById("error");
+let errorMsg = document.getElementById("errorMsg");
+let Prev = document.getElementById("Prev");
+let Next = document.getElementById("Next");
+
+let pageNumber = 1;
+const pageLimit = 10;
+
+let getData = false;
+const tableHeader = [
+  { Title: "", Url: "1654", Avatar: "Parco", Action: "", View: "" }
+];
 
 function generateTableHead(table, data) {
   let thead = table.createTHead();
@@ -12,20 +24,29 @@ function generateTableHead(table, data) {
     row.appendChild(th);
   }
 }
-
 function generateTable(table, data) {
   for (let element of data) {
     let row = table.insertRow();
     for (key in element) {
       let cell = row.insertCell();
       let cellContent = null;
-
-      console.log("key", element[key]);
       if (key === "avatar") {
         let img = document.createElement("img");
         img.src = element[key];
-
         cellContent = img;
+      } else if (key === "buttonDelete") {
+        let buttonDel = document.createElement("button");
+        let buttontextDel = document.createTextNode("delete");
+        buttonDel.appendChild(buttontextDel);
+        buttonDel.setAttribute("class", "delButton");
+        cellContent = buttonDel;
+      } else if (key === "buttonview") {
+        let buttonVie = document.createElement("button");
+        let buttontextDel = document.createTextNode("View");
+        buttonVie.appendChild(buttontextDel);
+        buttonVie.setAttribute("class", "viewButton");
+        buttonVie.classList.add("warrior");
+        cellContent = buttonVie;
       } else {
         cellContent = document.createTextNode(element[key]);
       }
@@ -36,25 +57,53 @@ function generateTable(table, data) {
 }
 
 let table = document.querySelector("table");
-let data = Object.keys(mountains[0]);
-let dataArr = [];
-fetch(baseUrl + "/news?page=1&limit=10")
-  .then(res => res.json())
-  .then(data => {
-    console.log("datat____", data);
-    let dataArr = data.map(({ title, url, avatar }) => {
-      return {
-        title,
-        url,
-        avatar
-      };
-    });
-    generateTable(table, dataArr);
-  })
-  .catch(error => {
-    console.log("hiiii", error);
-  });
 
+let data = Object.keys(tableHeader[0]);
 generateTableHead(table, data);
 
-var tr = document.getElementsByTagName("tr");
+const fetchSearchTopStories = (pageNumber, pageLimit = 0) => {
+  fetch(baseUrl + `/news?page= ${pageNumber}&limit=${pageLimit}`)
+    .then(res => res.json())
+    .then(data => {
+      let dataArr = data.map(
+        ({ title, url, avatar, buttonDelete, buttonview }) => {
+          return {
+            title,
+            url,
+            avatar,
+            buttonDelete,
+            buttonview
+          };
+        }
+      );
+      getData = true;
+      generateTable(table, dataArr);
+      if (data) {
+        errorId.style.display = "none";
+      }
+      if (!data) {
+        Next.style.display = "none";
+        Prev.style.display = "none";
+      }
+    })
+    .catch(error => {
+      text = document.createTextNode("Failed to fetch");
+      errorMsg.appendChild(text);
+      if (error) {
+        Next.style.display = "none";
+        Prev.style.display = "none";
+      }
+    });
+};
+window.addEventListener("load", fetchSearchTopStories(1, 10));
+
+Next.addEventListener("click", () => {
+  fetchSearchTopStories(pageNumber++, 10);
+});
+if (pageNumber <= 2) {
+  Prev.style.disabled = true;
+}
+Prev.addEventListener("click", () => {
+  fetchSearchTopStories(pageNumber--, 10);
+});
+
